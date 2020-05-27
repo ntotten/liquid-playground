bcbd1ab80ac61b9ad042150892f68a1e676c8abf
Nathan Totten <nathan@ntotten.com>
2020-05-27 15:27:10 -0400

    templates

--- src/samples/index.js
+++ src/samples/index.js
@@ -1,15 +1,15 @@
 var PLAY_SAMPLES = [
   {
-    chapter: "Creating the editor",
-    name: "Hello World",
-    id: "creating-the-editor-hello-world",
-    path: "creating-the-editor/hello-world",
+    chapter: "Basics",
+    name: "Operators",
+    id: "basics-operators",
+    path: "basics/operators",
   },
   {
-    chapter: "Creating the editor",
-    name: "Hello World 2",
-    id: "creating-the-editor-editor-hello-world-2",
-    path: "creating-the-editor/hello-world-2",
+    chapter: "Basics",
+    name: "Truthy and Falsy",
+    id: "basics-truthy-falsy",
+    path: "basics/truthy-falsy",
   },
 ];
 
