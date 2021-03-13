8d781f5d69b03bac6ba48e55cf110eff2e316bee
Nathan Totten <nathan@ntotten.com>
2021-03-13 10:45:56 -0500

    more samples

--- src/samples/index.js
+++ src/samples/index.js
@@ -1,6 +1,12 @@
 var PLAY_SAMPLES = [
   {
     chapter: "Basics",
+    name: "Introduction",
+    id: "basics-introduction",
+    path: "basics/introduction",
+  },
+  {
+    chapter: "Basics",
     name: "Operators",
     id: "basics-operators",
     path: "basics/operators",
@@ -10,6 +16,42 @@ var PLAY_SAMPLES = [
     name: "Truthy and Falsy",
     id: "basics-truthy-falsy",
     path: "basics/truthy-falsy",
+  },
+  {
+    chapter: "Basics",
+    name: "Types",
+    id: "basics-types",
+    path: "basics/types",
+  },
+  {
+    chapter: "Tags",
+    name: "Comment",
+    id: "tags-comment",
+    path: "tags/comment",
+  },
+  {
+    chapter: "Tags",
+    name: "Control Flow",
+    id: "tags-controlflow",
+    path: "tags/control-flow",
+  },
+  {
+    chapter: "Tags",
+    name: "Iteration",
+    id: "tags-iteration",
+    path: "tags/iteration",
+  },
+  {
+    chapter: "Tags",
+    name: "Raw",
+    id: "tags-raw",
+    path: "tags/raw",
+  },
+  {
+    chapter: "Tags",
+    name: "Variable",
+    id: "tags-variable",
+    path: "tags/variable",
   },
 ];
 
